97cdf98db8f64cf2a01b80e640d069b146461363
Sergey Kot <bytekot@gmail.com>
2021-02-05 23:58:36 +0300

    Add click handlers

--- src/view/view.jsx
+++ src/view/view.jsx
@@ -2,23 +2,45 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GameMap from '../core/Map';
 
-const SapperMapView = ({ map }) => (
-	<div className="container">
-        {
-            map.map(row => (
-                <div className="row">
-                    {
-                        row.map(cellValue => (
-                            <div className={`cell ${cellValue === 'mine' ? 'mine' : ''}`}>
-                                <span>{cellValue !== 'mine' ? cellValue : '💣'}</span>
-                            </div>
-                        ))
-                    }
-                </div>
-            ))
-    	}
-    </div>
-);
+const onClick = event => {
+    if (event.target.classList.contains('mask')) {
+        event.target.classList.remove('mask');
+    }
+};
+
+const onContextMenu = event => {
+    event.preventDefault();
+
+    if (event.target.classList.contains('mask')) {
+        event.target.classList.add('flag');
+    }
+};
+
+const SapperMapView = ({ map }) => {
+	return (
+        <div className="container">
+            {
+                map.map((row, rowIndex) => (
+                    <div className="row">
+                        {
+                            row.map((cellValue, cellIndex) => (
+                                <div
+                                    x={cellIndex}
+                                    y={rowIndex}
+                                    className={`mask cell ${cellValue === 'mine' ? 'mine' : ''}`}
+                                    onClick={onClick}
+                                    onContextMenu={onContextMenu}
+                                >
+                                    {cellValue !== 'mine' ? cellValue : '💣'}
+                                </div>
+                            ))
+                        }
+                    </div>
+                ))
+            }
+        </div>
+    );
+};
 
 const gameMap = new GameMap({
 	width: 10,
